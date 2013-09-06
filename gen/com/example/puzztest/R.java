23d62eaf7601a91afd9a5dcbd1bef8c7ece1b5ee
zoeas <zoeas@puzz>
2013-09-06 09:23:47 +0900

    파일추가 테스트

--- gen/com/example/puzztest/R.java
+++ gen/com/example/puzztest/R.java
@@ -25,11 +25,12 @@ public final class R {
         public static final int out01=0x7f020001;
         public static final int out02=0x7f020002;
         public static final int out03=0x7f020003;
-        public static final int puzz_body=0x7f020004;
-        public static final int puzz_hand=0x7f020005;
-        public static final int puzz_head01=0x7f020006;
-        public static final int puzz_head02=0x7f020007;
-        public static final int puzz_mstone=0x7f020008;
+        public static final int puzz=0x7f020004;
+        public static final int puzz_body=0x7f020005;
+        public static final int puzz_hand=0x7f020006;
+        public static final int puzz_head01=0x7f020007;
+        public static final int puzz_head02=0x7f020008;
+        public static final int puzz_mstone=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
